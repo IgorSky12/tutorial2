c978773e50cf1ba57f802c4dddfdb2d68efa4094
igorsky12 <igor.skoblikov@stud.hs-ruhrwest.de>
2023-01-23 06:14:08 +0100

    new class questions, pub_date

--- polls/views.py
+++ polls/views.py
@@ -20,17 +20,25 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
+    def vote(request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
+            return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+            })
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+class DetailView(generic.DetailView):
+    ...
+    def get_queryset(self):
+        
+        # Excludes any questions that aren't published yet.
+        
+        return Question.objects.filter(pub_date__lte=timezone.now())
